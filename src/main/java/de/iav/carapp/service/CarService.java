c8abdeaae80a417c0274f60d7431288429c06499
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-05-27 07:16:25 +0200

    add carService

--- src/main/java/de/iav/carapp/service/CarService.java
+++ src/main/java/de/iav/carapp/service/CarService.java
@@ -1,4 +1,45 @@
 package de.iav.carapp.service;
 
+import de.iav.carapp.model.Car;
+import de.iav.carapp.repository.CarReopsitory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+
+    private CarReopsitory carReopsitory;
+
+    public CarService(CarReopsitory carReopsitory) {
+        this.carReopsitory = carReopsitory;
+    }
+
+    public List<Car> getAllCars() {
+        return carReopsitory.getAllCars();
+    }
+
+    public Car getCarById(String id) {
+        return carReopsitory.getCarById(id);
+    }
+
+    public Car getCarByVin(String vin){
+        return carReopsitory.getCarByVin(vin);
+    }
+
+    public Car getCarByModel(String model){
+        return carReopsitory.getCarByModel(model);
+    }
+
+    public void addCar(Car car){
+        carReopsitory.addCar(car);
+    }
+
+    public void deleteCar(String id){
+        carReopsitory.deleteCar(id);
+    }
+
+    public Car updateCar(String id, Car car){
+        return carReopsitory.updateCar(id, car);
+    }
 }
