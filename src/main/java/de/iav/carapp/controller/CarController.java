d6e4ad1f7842088e1ac4ebeaeb805874f08a1442
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-05-27 07:33:05 +0200

    add carController

--- src/main/java/de/iav/carapp/controller/CarController.java
+++ src/main/java/de/iav/carapp/controller/CarController.java
@@ -1,4 +1,53 @@
 package de.iav.carapp.controller;
 
+import de.iav.carapp.model.Car;
+import de.iav.carapp.service.CarService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cars")
 public class CarController {
+
+    private CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public List<Car> getAllCars(){
+        return carService.getAllCars();
+    }
+
+    @GetMapping("/getcar/byid/{id}")
+    public Car getCarById(@PathVariable String id){
+        return carService.getCarById(id);
+    }
+
+    @GetMapping("/getcar/byvin/{vin}")
+    public Car getCarByVin(@PathVariable String vin){
+        return carService.getCarByVin(vin);
+    }
+
+    @GetMapping("getcar/bymodel/{model}")
+    public Car getCarByModel(@PathVariable String model){
+        return carService.getCarByModel(model);
+    }
+
+    @PostMapping("/addcar")
+    public void addCar(Car car){
+        carService.addCar(car);
+    }
+
+    @PostMapping("/updatecar/{id},{car}")
+    public Car updateCar(@PathVariable String id, @PathVariable Car car){
+        return carService.updateCar(id ,car);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCar(@PathVariable String id){
+         carService.deleteCar(id);
+    }
 }
